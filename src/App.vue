81de1c6b85fff79ce0702f3c5c2a36995a154cf5
eriker75 <eribertmarquez75@gmail.com>
2022-08-08 18:04:18 -0400

    create empty counter component, delete helloWorld

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CounterComponent></CounterComponent>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CounterComponent from './components/Counter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CounterComponent
   }
 }
 </script>
